3f622217d1e392a38832c651813b79ea6d3420c3
ncortesignaccolo-sudo <ncortesignaccolo@lin.safanet.es>
2026-03-03 17:10:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// Scroll reveal más elegante
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
+const elements = document.querySelectorAll(".service-card, .glass-card");
+
+const observer = new IntersectionObserver(entries=>{
+  entries.forEach(entry=>{
     if(entry.isIntersecting){
       entry.target.style.opacity = 1;
       entry.target.style.transform = "translateY(0)";
     }
   });
-},{
-  threshold:0.2
-});
+},{threshold:0.2});
 
-document.querySelectorAll(".reveal").forEach(el=>{
+elements.forEach(el=>{
+  el.style.opacity = 0;
+  el.style.transform = "translateY(50px)";
+  el.style.transition = "all .8s ease";
   observer.observe(el);
 });
